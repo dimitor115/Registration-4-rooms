7acfb580b16d31c9a656633a4048dbfa0b9d229d
Maciej Kopeć <dimitor115@gmail.com>
2019-07-29 13:11:29 +0200

    init

---
+++ backed/src/protectedRoutes.ts
@@ -0,0 +1,17 @@
+import { SwaggerRouter } from 'koa-swagger-decorator';
+import controller = require('./controller');
+
+const protectedRouter = new SwaggerRouter();
+
+
+// Swagger endpoint
+protectedRouter.swagger({
+    title: 'node-typescript-koa-rest',
+    description: 'API REST using NodeJS and KOA framework, typescript. TypeORM for SQL with class-validators. Middlewares JWT, CORS, Winston Logger.',
+    version: '1.5.0'
+});
+
+// mapDir will scan the input dir, and automatically call router.map to all Router Class
+protectedRouter.mapDir(__dirname);
+
+export { protectedRouter };

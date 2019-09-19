1537bb4d137ceb134fadc9ce6ee7b0065e2badcd
Maciej Kopeć <dimitor115@gmail.com>
2019-09-19 21:25:18 +0200

    Add custom styles

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,9 +5,12 @@ import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
+
+import '@/assets/style.scss'
 
 new Vue({
   router,

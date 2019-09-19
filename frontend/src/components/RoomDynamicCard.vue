8a85154740c964ac96a68937fabc83e35c99db87
Maciej Kopeć <dimitor115@gmail.com>
2019-09-19 22:14:14 +0200

    Improve styles

--- frontend/src/components/RoomDynamicCard.vue
+++ frontend/src/components/RoomDynamicCard.vue
@@ -3,7 +3,9 @@
     <template slot="header">
       <span @click="expandRoom">{{room.name}}</span>
     </template>
-    <div @click="expandRoom">Miejsca: {{room.students.length}} / {{room.size}}</div>
+
+    <div class="places-container">Miejsca: {{room.students.length}} / {{room.size}}</div>
+
     <transition name="fade">
       <div v-if="showDetails">
         <template v-for="(student, idx) in room.students">
@@ -14,53 +16,90 @@
         </template>
       </div>
     </transition>
+
+    <div class="dynamic-card-footer" @click="expandRoom">
+      <template v-if="!showDetails">
+        <i class="el-icon-caret-bottom"></i>
+        <span class="expand-text">Rozwiń</span>
+      </template>
+      <template v-else>
+        <i class="el-icon-caret-top"></i>
+        <span class="expand-text">Zwiń</span>
+      </template>
+    </div>
   </el-card>
 </template>
 
 <script lang="ts">
-import Vue, { PropType } from 'vue'
-import { IRoom } from '@/models/IRoom'
-import StudentInRoomForm from './StudentInRoomForm.vue'
-import { IStudent } from '../models/IStudent'
-import { Actions } from '../shared/Actions'
+import Vue, { PropType } from "vue";
+import { IRoom } from "@/models/IRoom";
+import StudentInRoomForm from "./StudentInRoomForm.vue";
+import { IStudent } from "../models/IStudent";
+import { Actions } from "../shared/Actions";
 export default Vue.extend({
-  name: 'room-dynamic-card',
+  name: "room-dynamic-card",
   components: { StudentInRoomForm },
   props: {
     room: {
       type: Object as PropType<IRoom>,
-      required: true,
-    },
+      required: true
+    }
   },
   data: () => ({
     showDetails: false
   }),
   computed: {
     restPlaces(): number {
-      return this.room.size - this.room.students.length
-    },
+      return this.room.size - this.room.students.length;
+    }
   },
   methods: {
     registerStudent(student: IStudent) {
-      console.log({...student})
-      this.$store.dispatch(Actions.REGISTER_STUDENT, {roomId: this.room._id, student})
+      console.log({ ...student });
+      this.$store.dispatch(Actions.REGISTER_STUDENT, {
+        roomId: this.room._id,
+        student
+      });
     },
     removeStudent(student: IStudent) {
-      this.$store.dispatch(Actions.REMOVE_STUDENT, {roomId: this.room._id, student, removedBy:"me"})
+      this.$store.dispatch(Actions.REMOVE_STUDENT, {
+        roomId: this.room._id,
+        student,
+        removedBy: "me"
+      });
     },
     expandRoom() {
-      this.showDetails = this.showDetails ? false : true
-    },
-  },
-})
+      this.showDetails = this.showDetails ? false : true;
+    }
+  }
+});
 </script>
 <style lang="scss">
-.object-card {
-  margin: 20px 0;
-  background-color: rgb(221, 218, 218);
-  .el-card__header {
-    padding: 12px 20px;
+.dynamic-card-footer {
+  margin: 15px -20px -20px -20px;
+  padding: 16px 0 12px 0;
+  border-top: 1px solid #ebeef5;
+  color: #d3dce6;
+  cursor: pointer;
+  .expand-text {
+    display: none;
   }
+  &:hover {
+    background-color: #f9fafc;
+  }
+}
+
+.object-card:hover .dynamic-card-footer {
+  color: #1989fa;
+  .expand-text {
+    display: inline;
+    margin-left: 5px;
+    font-size: 16px;
+  }
+}
+
+.places-container{
+  padding: 17px;
 }
 
 .fade-enter-active,

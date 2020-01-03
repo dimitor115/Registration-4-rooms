1619d70331940866c40e0f86b93228cd1e0002b5
Maciej Kopeć <dimitor115@gmail.com>
2020-01-03 03:09:26 +0100

    Add user avatar and logout button

---
+++ frontend/src/components/UserAvatar.vue
@@ -0,0 +1,41 @@
+<template>
+     <div class="user">
+        <el-popover placement="bottom" width="200" trigger="click">
+          <el-avatar
+            slot="reference"
+            icon="el-icon-user-solid"
+            shape="square"
+            :size="40"
+            :src="$store.state.user.data.imageUrl"
+          ></el-avatar>
+          <div>
+          {{$store.state.user.data.name}}
+          <el-button type="danger" @click="logout">Wyloguj</el-button>
+          </div>
+        </el-popover>
+      </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    methods: {
+        async logout() {
+            const auth2 = window.gapi.auth2.getAuthInstance()
+            await auth2.signOut()
+            this.$router.push('/admin/login')
+        }
+    }
+})
+</script>
+<style lang="scss" scoped>
+ .user {
+    width: 50px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-end;
+    cursor: pointer;
+    .el-avatar:hover {
+      box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.2);
+    }
+  }
+</style>
--- frontend/src/views/administrator/AdminContainer.vue
+++ frontend/src/views/administrator/AdminContainer.vue
@@ -1,28 +1,37 @@
 <template>
   <div>
-    <el-menu
-      router
-      :default-active="activePage"
-      mode="horizontal"
-      class="wrapper"
-    >
-      <el-menu-item index="live">Zapisy live</el-menu-item>
-      <el-menu-item index="zarzadzanie">Zarządzanie pokojami</el-menu-item>
-      <el-menu-item index="zarzadzanie">Zarządzanie administratorami</el-menu-item>
-    </el-menu>
+    <div class="menu-container">
+      <el-menu class="menu" router :default-active="activePage" mode="horizontal">
+        <el-menu-item index="live">Zapisy live</el-menu-item>
+        <el-menu-item index="zarzadzanie">Zarządzanie pokojami</el-menu-item>
+        <el-menu-item index="zarzadzanie">Zarządzanie administratorami</el-menu-item>
+      </el-menu>
+      <user-avatar></user-avatar>
+    </div>
     <router-view></router-view>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import UserAvatar from '@/components/UserAvatar.vue'
 
 export default Vue.extend({
   name: "AdminContainer",
+  components: {UserAvatar},
   computed: {
-      activePage() {
-        return this.$router.currentRoute.path.split('/').splice(-1)[0]
-      }
+    activePage() {
+      return this.$router.currentRoute.path.split("/").splice(-1)[0];
+    }
   }
 });
 </script>
+<style lang="scss" scoped>
+.menu-container {
+  display: flex;
+  justify-content: space-between;
+  .menu {
+    width: 91%;
+  }
+}
+</style>

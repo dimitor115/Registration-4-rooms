9fd8762c774225d30abe1818929a60cb46ce3e3b
dimitor115 <dimitor115@gmail.com>
2020-01-25 02:06:19 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ yarn build
 cd ..
 cp -r frontend/dist backend/dist/public
 scp -r  backend/dist/* nativeflow@s1.ct8.pl:domains/zapisy.wydarzenia.napwr.pl/public_nodejs/
-ssh nativeflow:Czworaczki1997@s1.ct8.pl "cd domains/zapisy.wydarzenia.napwr.pl/public_nodejs/;npm install;devil www restart zapisy.wydarzenia.napwr.pl"
+ssh nativeflow@s1.ct8.pl "cd domains/zapisy.wydarzenia.napwr.pl/public_nodejs/;npm install;devil www restart zapisy.wydarzenia.napwr.pl"
 
